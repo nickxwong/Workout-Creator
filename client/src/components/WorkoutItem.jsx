17a41bff5904fd6493e50144d3d89bb0ef7bd88a
Nick Wong <nicolaswong01@gmail.com>
2022-08-03 19:27:04 -0700

    Add functionality to remove exercises from workout

--- client/src/components/WorkoutItem.jsx
+++ client/src/components/WorkoutItem.jsx
@@ -13,12 +13,16 @@ export default function WorkoutItem ({ id, index, exercise, numSets, workout, se
     const [x, setNumSets] = useState(numSets);
 
     const addSet = () => {
-        const newIter = workout.map((val, i) => {
-            if (i == index) {
+        workout.forEach((val, i) => {
+            if (i === index) {
                 val.numSets++
                 setNumSets(val.numSets);
             }
         })
+    }
+
+    const removeExercise = () => {
+        setWorkout(workout.filter((cur, arr_index) => arr_index !== index));
     }
 
     const ref = useRef(null)
@@ -76,7 +80,7 @@ export default function WorkoutItem ({ id, index, exercise, numSets, workout, se
         <div className="workout-item" ref={ref} style={{opacity}} data-handler-id={handlerId}>
             <div className="info">
                 <h3>{exercise.exercise_name}</h3>
-                <p>Remove</p>    
+                <p className="remove-button" onClick={removeExercise}>Remove</p>    
             </div>
             <div className="exercise-sets">
                 {[...Array(x)].map((value, i) => {
@@ -89,44 +93,3 @@ export default function WorkoutItem ({ id, index, exercise, numSets, workout, se
         </div>
     )
 }
-
-// import { useState } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faPlus } from '@fortawesome/free-solid-svg-icons';
-// import '../css/WorkoutItem.css';
-// import ExerciseSet from '../js/ExerciseSet';
-
-// const WorkoutItem = ({index, exercise, num_sets, workout, setWorkout}) => {
-
-//     const addIcon = <FontAwesomeIcon icon={faPlus} />
-
-//     const [numSets, setNumSets] = useState(0);
-
-//     const removeExercise = () => {
-//         setWorkout(workout.filter((cur, arr_index) => arr_index !== index));
-//     }
-
-//     const addSet = () => {
-//         setNumSets(numSets + 1);
-//         console.log(workout);
-//     }
-    
-//     return (
-//         <div className="workout-item">
-//             <div className="exercise-info">
-//                 <h3>{exercise.exercise_name}</h3>
-//                 <p className="remove-button" onClick={removeExercise}>Remove</p>
-//             </div>
-//             <div className="workout-sets">
-//                 {[...Array(numSets)].map((value, index) => {
-//                     return(
-//                         <ExerciseSet key={index} index={index + 1}/>
-//                     );
-//                 })}
-//                 <div className="add-button" onClick={addSet}>{addIcon}</div>
-//             </div>
-//         </div>
-//     )
-// };
-
-// export default WorkoutItem;
