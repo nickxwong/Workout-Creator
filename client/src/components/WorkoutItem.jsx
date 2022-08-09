955b3ece9fc3dbe08141793c86a23104f8ec048e
Nick Wong <nicolaswong01@gmail.com>
2022-08-08 20:43:08 -0700

    Rewrite functionality to delete exercises from a workout

--- client/src/components/WorkoutItem.jsx
+++ client/src/components/WorkoutItem.jsx
@@ -4,17 +4,18 @@ import { useState, useRef } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
 import update from 'immutability-helper'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faX } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 // components
 import ExerciseSet from './ExerciseSet'
 
 export default function WorkoutItem ({ id, index, exercise, setsReps, workout, setWorkout, moveItem }) {
     
-    const xIcon = <FontAwesomeIcon icon={faX} />
+    const trashIcon = <FontAwesomeIcon icon={faTrash} />
     const addIcon = <FontAwesomeIcon icon={faPlus} />
     
     const [numSets, setNumSets] = useState(setsReps.length);
+    const [showRemove, setShowRemove] = useState(false)
 
     const addSet = () => {
         const length = workout[index].setsReps.length - 1
@@ -81,13 +82,14 @@ export default function WorkoutItem ({ id, index, exercise, setsReps, workout, s
 
     return (
         <div className="workout-item" ref={ref} style={{opacity}} data-handler-id={handlerId}>
-            <div className="info">
+            <div className="info" onMouseEnter={() => setShowRemove(true)} onMouseLeave={() => setShowRemove(false)}>
                 <h3>{exercise.exercise_name}</h3>
                 <div className="muscle-groups"> 
                     <p>{exercise.primary_muscle}</p>
                     {exercise.secondary_muscle != null && <p>{exercise.secondary_muscle}</p>}
                     {exercise.tertiary_muscle != null && <p>{exercise.tertiary_muscle}</p>}
       		    </div>
+                {showRemove && <div className="remove-button" onClick={removeExercise}>{trashIcon}</div>}
             </div>
             <div className="exercise-sets">
                 {[...Array(numSets)].map((value, i) => {
